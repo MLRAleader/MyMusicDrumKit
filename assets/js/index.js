a628a45df3c6bbf2bc408177af91351ce3ab821c
Axel <44190680+MLRAleader@users.noreply.github.com>
2026-01-15 06:20:13 +0100

    Refactor audio handling and event listeners
    I gave the user the ability to play with specific keyboard keys.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,43 @@
 
+function handleKeyPress() { 
+  window.addEventListener("keydown", function (event) {
+    console.log(event.key);
+    switch (event.key) {
+      case "w":
+        let tom1 = new Audio("./assets/sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "a":
+        var tom2 = new Audio("./assets/sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "s":
+        var tom3 = new Audio("./assets/sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "d":
+        var tom4 = new Audio("./assets/sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      case "j":
+        var snare = new Audio("./assets/sounds/snare.mp3");
+        snare.play();
+        break;
+      case "k":
+        var crash = new Audio("./assets/sounds/crash.mp3");
+        crash.play();
+        break;
+      case "l":
+        var kick = new Audio("./assets/sounds/kick-bass.mp3");
+        kick.play();
+        break
+      default:
+       this.alert("You did not press the correct key (w,a,s,d,j,k,l)!");
+    }
+  });
+}
+handleKeyPress();
+
 function handleClick() {
   for (let i = 0; i < document.querySelectorAll("button").length; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function () {
@@ -6,9 +45,7 @@ function handleClick() {
      switch (this.innerHTML) {
        case "w":
          let tom1 = new Audio("./assets/sounds/tom-1.mp3");
-        
          tom1.play();
-          
          break;
        case "a":
          var tom2 = new Audio("./assets/sounds/tom-2.mp3");
